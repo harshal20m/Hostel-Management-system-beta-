d6f6be35544787712fcfe8d9fa203fcabd95b29e
Harshal Mali <20harshalmali@gmail.com>
2023-10-28 23:25:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+
+
+
